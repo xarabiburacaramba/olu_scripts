a4ccb8438a9b9a5b72b50590167cf21a5f633ecd
Dima Kozuch <dmitrii@seznam.cz>
2019-08-16 10:27:58 +0200

    rozsireny priklad na stahovani dat z wfs sluzby

--- stahovani_dat_z_wfs_olu.py
+++ stahovani_dat_z_wfs_olu.py
@@ -1,16 +1,23 @@
 import requests
 
 #ohranicujici obdelnik v souradnicovem systemu EPSG:3857
-bbox='1637091,6434795,1637092,6434796'
+bbox='2019772,6384876,2040929,6372402'
+limit=5000
+offset=0
 
-folder='slozka_stazeni'
+folder='/slozka_stazeni/'
 
-url='http://gis.lesprojekt.cz/cgi-bin/mapserv?map=/home/dima/maps/olu/european_openlandusemap.map&service=WFS&VERSION=1.1.0&REQUEST=GetFeature&TYPENAME=olu_bbox&SRSNAME=EPSG:3857&BBOX=%s&outputformat=geojson' % bbox
+url='http://gis.lesprojekt.cz/cgi-bin/mapserv?map=/home/dima/maps/olu/european_openlandusemap.map&service=WFS&VERSION=1.1.0&REQUEST=GetFeature&TYPENAME=olu_bbox&SRSNAME=EPSG:3857&BBOX=%s&OFFSET=%s&LIMIT=%s&outputformat=geojson' % (bbox, offset, limit)
+
+i=0
 
 r=requests.get(url, stream=True)
 
-if r.status_code==200:
-    with open(folder, 'wb') as f:
+while len(r.text)>0 and r.status_code==200:
+    with open(folder+str(i)+'.json', 'wb') as f:
         f.write(r.content)
-else:
-    print(r.status_code)
+    offset+=limit
+    i+=1
+    url='http://gis.lesprojekt.cz/cgi-bin/mapserv?map=/home/dima/maps/olu/european_openlandusemap.map&service=WFS&VERSION=1.1.0&REQUEST=GetFeature&TYPENAME=olu_bbox&SRSNAME=EPSG:3857&BBOX=%s&OFFSET=%s&LIMIT=%s&outputformat=geojson' % (bbox, offset, limit)
+    r=requests.get(url, stream=True)
+    
